9f25fdc4f2fd5ed5cb4f5e96bcb310d4517a0ab6
Mustafa-Salah200 <139685902+Mustafa-Salah200@users.noreply.github.com>
2025-02-26 08:05:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,40 +15,21 @@ app.post("/ussd", (req, res) => {
   const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
   let response = "";
+  let user_name;
 
-  if (text == "") {
+  if (text === "") {
       // This is the first request. Note how we start the response with CON
-      response = `CON What would you like to check
-      Enter Your Name`
-      console.log(text);
-    } else if (text === "") {
+      response = `  Enter Your Name`
+      user_name = text;
+    } else if (text !== "") {
         // Business logic for first level response
-        response = `CON Choose account information you want to view
-        1. Account number`;
         console.log(text);
-  } else if (text === "1") {
-    // Business logic for first level response
-    response = `CON Choose account information you want to view
-        1. Account number`;
+        response = `Your Name: ${text}\n`;
+        response += `1- Account number`;
   } 
-  else if (text === "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    response = `END Your phone number is ${phoneNumber}`;
-  } else if (text === "1*1") {
-    // This is a second level response where the user selected 1 in the first instance
-    const accountNumber = "ACC100101";
-    // This is a terminal request. Note how we start the response with END
-    response = `END Your account number is ${accountNumber}`;
-  } else if (text === "mustafa") {
-    // This is a second level response where the user selected 1 in the first instance
-    const accountNumber = "Mustafa";
-    // This is a terminal request. Note how we start the response with END
-    response = `Your Name is ${accountNumber}`;
-  }
 
   // Send the response back to the API
   res.set("Content-Type: text/plain");
-   console.log(text);
+  console.log(text);
   res.send(response);
 });
