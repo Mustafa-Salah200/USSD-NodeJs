aaf83ba79a01c1f9a68916a7488750d9ab3b1a48
Mustafa-Salah200 <mustafaslahtoofy@gmail.com>
2025-02-26 05:52:32 -0500

    update something in the code

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const port = 9000
+
+app.listen(port, () => {
+    console.log(`USSD API listening at => http://localhost:${port}`);
+});
+
 app.post('/ussd', (req, res) => {
     // Read the variables sent via POST from our API
     const {
@@ -20,7 +25,8 @@ app.post('/ussd', (req, res) => {
         // This is the first request. Note how we start the response with CON
         response = `CON What would you like to check
         1. My account
-        2. My phone number`;
+        2. Enter Your Email
+        3. My phone number`;
     } else if ( text == '1') {
         // Business logic for first level response
         response = `CON Choose account information you want to view
@@ -34,13 +40,14 @@ app.post('/ussd', (req, res) => {
         const accountNumber = 'ACC100101';
         // This is a terminal request. Note how we start the response with END
         response = `END Your account number is ${accountNumber}`;
+    } else if ( text == 'mustafa') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'Mustafa';
+        // This is a terminal request. Note how we start the response with END
+        response = `Your Name is ${accountNumber}`;
     }
 
     // Send the response back to the API
     res.set('Content-Type: text/plain');
     res.send(response);
-});
-
-app.listen(port, () => {
-    console.log(`USSD API listening at http://localhost:${port}`);
 });
